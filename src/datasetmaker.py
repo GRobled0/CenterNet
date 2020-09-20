f2dc04f06bda2845c843350a5c2a7afcc0277f1e
grobled0 <grobled0@hotmail.com>
2020-09-20 22:27:59 +0200

    20-9-2020: datasetmaker

--- src/datasetmaker.py
+++ src/datasetmaker.py
@@ -1,4 +1,4 @@
-#python datasetmaker.py ctdet
+#python datasetmaker.py
 #hay que aplicar parametros intrinsecos y extrinsecos de la camara
 
 #ls -v | cat -n | while read n f; do mv -n "$f" "$n.ext"; done  para cambiar los nombres de los archivos de una carpeta por numeros consecutivos
@@ -8,7 +8,7 @@ import _init_paths
 import os
 import cv2
 import csv
-
+import argparse
 import json
 
 import numpy as np
@@ -19,10 +19,7 @@ from detectors.detector_factory import detector_factory
 from camera_params import *
 
 image_ext = ['jpg', 'jpeg', 'png', 'webp', 'ppm', 'pgm']
-threshold = 0.85
-
-low_percentile = 25
-high_percentile = 75 #el blanco contiene menos información, a partir de 10 m todos los valores son identicos
+threshold = 0.85 #limite para decidir que es una silla
 
 def carga_imagenes(directorio):
   if os.path.isdir(directorio):
@@ -38,65 +35,63 @@ def carga_imagenes(directorio):
 
 
 
-def guardar_imagen(directorio, id1, id2):
-  out_dir = []
-  out_dir.append(directorio)
-  out_dir.append('/')
-  out_dir.append(str(id1))
-  out_dir.append('_')
-  out_dir.append(str(id2))
-  out_dir.append('.png')
-  out = ''.join(out_dir)
-  return out 
+
+#la siguiente funcion cambia el valor de la media de los pixels por la distancia en metros
+def distabs(value):
+  value_swap = np.array([value], dtype = np.uint16)
+  value_swap.byteswap(True)
+
+  imgDepthAbs = depthParam1 / (depthParam2 - value_swap[0]); 
+
+  return imgDepthAbs
 
 
-def distabs_media(low_thres, high_thres, img):
-  i_max, j_max = img.shape
-  img_swap = np.uint16(img.copy())
-  img_swap.byteswap(True)
+def check(bbox, shape):
+  checked = bbox
+  thresh = 0
 
-  m = 0
-  n = 0
+  for i in range(0,4):
+    if checked[i] < 0:
+      checked[i] = 0
+    if i % 2 == 0:
+       if checked[i] > shape[1]:
+         checked[i] = shape[1]
+    else:
+      if checked[i] > shape[0]:
+         checked[i] = shape[0]
 
-  for i in range (0, i_max):
-     for j in range (0, j_max):
-
-       if (img[i][j] < high_thres) and  (img[i][j] > low_thres):
-         imgDepthAbs = depthParam1 / (depthParam2 - img_swap[i][j]); 
-
-         if imgDepthAbs > maxDepth:
-           imgDepthAbs = maxDepth   
-    
-         if imgDepthAbs < 0:
-           imgDepthAbs = 0
-
-         m = m + imgDepthAbs
-         n = n + 1
-  if n == 0:
-    n = 1
-  return m/n
+  return checked
 
 
 
-def dataset_maker(opt):
-  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
-  opt.debug = max(opt.debug, 1)
+def analyze_data(opt, image_names, image_names_d):
   Detector = detector_factory[opt.task]
   detector = Detector(opt)
 
   document_data = {'images': [], 'annotations': [], 'categories': []}
   info_image = dict()
-  
-  image_names = carga_imagenes('/home/guillermo/Escritorio/dataset/reception_rooms/images_3')
-  image_names_d = carga_imagenes('/home/guillermo/Escritorio/dataset/reception_rooms/depth_3') #carga imagenes d
 
   i = 0
   j = 0
+  percentage_print = 0
   for image_name in image_names:  #detecta en cada imagen
     ret = detector.run(image_name)
-    i = i + 1 #cada imagen rgb tiene en la misma posicion su imagen d
+    i = i + 1
 
-    print('imagen:' + str(i))
+    percentage = int(i*100/len(image_names))
+
+    if percentage >= percentage_print:
+       string = "["
+       for x in range(int(100/2.5)):
+         if x <= int(percentage/2.5):
+           string = string + "|"
+         else:
+           string = string + "-"
+       string = string + "] "
+       print(string + str(percentage) + '%')
+       percentage_print = percentage_print + 2.5    
+
+    #print('imagen:' + str(i))
 
     image_info = {'file_name': '{}'.format(image_name),
                   'id': i,
@@ -106,15 +101,15 @@ def dataset_maker(opt):
     for chair in ret['results'][57]: #57 es el id de las sillas en el coco dataset
       if chair[4] > threshold:
         j = j + 1
-        img = cv2.imread(image_names_d[i+3],-1) #cambiar si duese necesario, +3 parece que en dataset estan movidas
-        crop_img = img[int(chair[1]):int(chair[3]), int(chair[0]):int(chair[2])]
+        img = cv2.imread(image_names_d[i-1],-1)
 
-        low_thres = np.percentile(crop_img, low_percentile)
-        high_thres = np.percentile(crop_img, high_percentile)
+        checked = check(chair[0:4], img.shape) #comprobar que el bbox no se sale de la imagen
 
-        dist = distabs_media(low_thres, high_thres, crop_img.copy())
+        crop_img = img[int(checked[1]):int(checked[3]), int(checked[0]):int(checked[2])]
+
+        dist = distabs(np.percentile(crop_img, 0.5)) #la mediana caracterizará la distancia
         if dist == 0:
-          dist = 0.01 #parece que sino da error
+          dist = 0.001 #parece que sino da error
 
         annon_info= {'image_id': i,
                      'id': int(len(document_data['annotations']) + 1),
@@ -125,9 +120,41 @@ def dataset_maker(opt):
 
         document_data['annotations'].append(annon_info)
 
-  with open('dataset_3.json', 'w') as file:
+  return document_data
+
+
+
+def dataset_maker(opt):
+  os.environ['CUDA_VISIBLE_DEVICES'] = opt.gpus_str
+  opt.debug = max(opt.debug, 1)
+  opt.task = 'ctdet'
+
+  path_dataset = os.path.join(os.path.dirname(os.getcwd()), 'data/custom')
+  path_images = os.path.join(path_dataset, 'images')
+
+
+  #json del dataset para training
+  image_names = carga_imagenes(os.path.join(path_images, 'rgb'))
+  image_names_d = carga_imagenes(os.path.join(path_images, 'd'))
+
+  print("Dataset principal: ")
+  document_data = analyze_data(opt, image_names, image_names_d)
+  
+  with open(os.path.join(path_dataset, 'dataset.json'), 'w') as file:
     json.dump(document_data, file)
 
+  #json del test
+  path_test = os.path.join(path_dataset, 'images_test')
+
+  image_names = carga_imagenes(os.path.join(path_test, 'rgb'))
+  image_names_d = carga_imagenes(os.path.join(path_test, 'd'))
+
+  print("Dataset test:")
+  document_data = analyze_data(opt, image_names, image_names_d)
+  
+  with open(os.path.join(path_dataset, 'dataset_test.json'), 'w') as file:
+    json.dump(document_data, file)
+  
 
         
 if __name__ == '__main__':
